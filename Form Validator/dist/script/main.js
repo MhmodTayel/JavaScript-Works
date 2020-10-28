a8e1dabb7dffd09383f84128f1a40701fbee7eba
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-10-28 22:45:59 +0200

    done with validation

--- Form Validator/dist/script/main.js
+++ Form Validator/dist/script/main.js
@@ -1,3 +1,6 @@
+/* Note: classList.add -> adds error and success togther 
+so we shoud use className and put the right classes */
+
 const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
@@ -6,19 +9,23 @@ const password2 = document.getElementById("password2");
 
 function showError(input, message) {
   const formControl = input.parentElement;
+  formControl.className = "form-control error";
   const small = formControl.querySelector("small");
   small.innerText = message;
-  formControl.classList.add("error");
 }
 
 function showSuccess(input) {
   const formControl = input.parentElement;
-  formControl.classList.add("success");
+  formControl.className = "form-control success";
 }
 
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 
 function checkRequired(inputArr) {
@@ -33,7 +40,30 @@ function checkRequired(inputArr) {
   });
 }
 
+function checkLenght(input, min, max) {
+  const formControl = input.parentElement;
+  const label = formControl.querySelector("label");
+  if (input.value.length < min) {
+    showError(input, `${label.innerText} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${label.innerText} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Password do not match");
+  }
+}
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   checkRequired([username, email, password, password2]);
+  checkLenght(username, 3, 15);
+  checkLenght(password, 6, 25);
+  checkLenght(password2, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
 });
