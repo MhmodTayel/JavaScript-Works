3278b2bc63e9b0b43d3757715f1c02b588b07a3b
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-10-28 21:26:28 +0200

    dispaly error in username

---
+++ Form Validator/dist/script/main.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+  formControl.classList.add("error");
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+});
