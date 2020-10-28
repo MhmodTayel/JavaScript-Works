fac2fdfed58dc2e52158adc29fb7ddf1f1e4e17a
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-10-28 21:39:18 +0200

    simple Validation

--- Form Validator/dist/script/main.js
+++ Form Validator/dist/script/main.js
@@ -14,7 +14,11 @@ function showError(input, message) {
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.classList.add("success");
+}
 
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 form.addEventListener("submit", function (e) {
@@ -23,5 +27,25 @@ form.addEventListener("submit", function (e) {
     showError(username, "Username is required");
   } else {
     showSuccess(username);
+  }
+  //
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+  //
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(email);
+  }
+  //
+  if (password2.value === "") {
+    showError(password2, "password 2 is required");
+  } else {
+    showSuccess(password2);
   }
 });
