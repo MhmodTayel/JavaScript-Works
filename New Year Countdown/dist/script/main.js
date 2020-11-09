29d83b09c7598c91f602b1b2f105d881ff4c3c07
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-11-09 18:48:35 +0200

    New Year Countdown

---
+++ New Year Countdown/dist/script/main.js
@@ -0,0 +1,51 @@
+const year = document.getElementById("year");
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+const loading = document.getElementById("loading");
+
+const currentYear = new Date().getFullYear();
+const newYearTime = new Date(`January 01 ${currentYear + 1}   00:00:00`);
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  days.innerHTML = d;
+  if (h < 10) {
+    hours.innerHTML = `0${h}`;
+  } else {
+    hours.innerHTML = h;
+  }
+  if (m < 10) {
+    minutes.innerHTML = `0${m}`;
+  } else {
+    minutes.innerHTML = m;
+  }
+  if (s < 10) {
+    seconds.innerHTML = `0${s}`;
+  } else {
+    seconds.innerHTML = s;
+  }
+}
+
+setInterval(updateCountdown, 1000);
+
+year.innerText = `${newYearTime.getFullYear()}`;
+countdown.style.display = "none";
+year.style.display = "none";
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    countdown.style.display = "flex";
+    year.style.display = "block";
+    loading.style.display= 'none'
+  }, 1000);
+});
