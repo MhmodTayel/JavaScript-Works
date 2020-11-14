b8e779ccb76ad161ac0e027ab35a904d8bac22fa
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-11-14 16:23:46 +0200

    simple JSON fetch

--- Fetch API/dist/script/main.js
+++ Fetch API/dist/script/main.js
@@ -3,9 +3,25 @@ const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
 
 btn1.addEventListener("click", getText);
+btn2.addEventListener("click", getJSON);
 
 function getText() {
-  fetch("text.txt").then((res) => {
-    console.log(res);
-  });
+  fetch("text.txt")
+    .then((res) => {
+      return res.text();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+function getJSON() {
+  fetch("json.JSON")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.title);
+      console.log(data.body);
+    });
 }
