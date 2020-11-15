3558ce064a054fb1044383d76deb3797fa8c938c
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-11-16 00:08:48 +0200

    animate battery

--- Animate Font Awesome Icons/dist/script/main.js
+++ Animate Font Awesome Icons/dist/script/main.js
@@ -7,3 +7,23 @@ function breakChain() {
 }
 
 setInterval(breakChain, 2000);
+
+function batteryLoad() {
+  let battery = document.getElementById('battery');
+  battery.className = 'fas fa-battery-empty';
+  setTimeout(()=> {
+    battery.className = 'fas fa-battery-quarter'
+  },1000)
+  setTimeout(()=> {
+    battery.className = 'fas fa-battery-half'
+  },2000)
+  setTimeout(()=> {
+    battery.className = 'fas fa-battery-three-quarters'
+  },3000)
+  setTimeout(()=> {
+    battery.className = 'fas fa-battery-full'
+  },4000)
+}
+
+setInterval(batteryLoad,4000)
+
