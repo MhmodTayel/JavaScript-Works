4973f305ed796b302a9a30112b1c2b96fcab5e1f
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-11-16 00:12:11 +0200

    animate hour

--- Animate Font Awesome Icons/dist/script/main.js
+++ Animate Font Awesome Icons/dist/script/main.js
@@ -9,21 +9,35 @@ function breakChain() {
 setInterval(breakChain, 2000);
 
 function batteryLoad() {
-  let battery = document.getElementById('battery');
-  battery.className = 'fas fa-battery-empty';
-  setTimeout(()=> {
-    battery.className = 'fas fa-battery-quarter'
-  },1000)
-  setTimeout(()=> {
-    battery.className = 'fas fa-battery-half'
-  },2000)
-  setTimeout(()=> {
-    battery.className = 'fas fa-battery-three-quarters'
-  },3000)
-  setTimeout(()=> {
-    battery.className = 'fas fa-battery-full'
-  },4000)
+  let battery = document.getElementById("battery");
+  battery.className = "fas fa-battery-empty";
+  setTimeout(() => {
+    battery.className = "fas fa-battery-quarter";
+  }, 1000);
+  setTimeout(() => {
+    battery.className = "fas fa-battery-half";
+  }, 2000);
+  setTimeout(() => {
+    battery.className = "fas fa-battery-three-quarters";
+  }, 3000);
+  setTimeout(() => {
+    battery.className = "fas fa-battery-full";
+  }, 4000);
 }
 
-setInterval(batteryLoad,4000)
+setInterval(batteryLoad, 4000);
 
+function hourGlassLoad() {
+  let hour = document.getElementById("hour");
+  setTimeout(() => {
+    hour.className = "fas fa-hourglass-start";
+  }, 1000);
+  setTimeout(() => {
+    hour.className = "fas fa-hourglass-half";
+  }, 2000);
+  setTimeout(() => {
+    hour.className = "fas fa-hourglass-end";
+  }, 3000);
+}
+
+setInterval(hourGlassLoad, 3000);
