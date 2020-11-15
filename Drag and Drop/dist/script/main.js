866c138deecfb8cf3822293c8179c03fda3ee773
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-11-16 00:50:50 +0200

    done with drag

---
+++ Drag and Drop/dist/script/main.js
@@ -0,0 +1,16 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => {
+    this.className = "invisible";
+  }, 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
