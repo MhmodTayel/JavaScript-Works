ab56154579c4e38fcbe78095bc77e334405d5250
mhmodTayel <mhmodtayel98@gmail.com>
2021-01-02 01:14:44 +0200

    get the keycode of any key

---
+++ day  11 - KeyCodes/dist/script/main.js
@@ -0,0 +1,26 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", keyEmitter);
+
+function keyEmitter(e) {
+  insert.innerHTML = `
+
+    <div class="keycode-display">
+    ${e.keyCode}
+   
+ </div>
+   
+ <div class="key">
+ ${e.key === " " ? "Space" : e.key}
+ <small>event.key</small>
+</div>
+ <div class="key">
+ ${e.keyCode}
+    <small>event.keyCode</small>
+ </div>
+ <div class="key">
+ ${e.code}
+    <small>event.code</small>
+</div>
+    `;
+}
