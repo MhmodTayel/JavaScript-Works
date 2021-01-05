1cbd860ecd84d936392e017c0abde31b79a663b6
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-06 01:14:47 +0200

    Add api cred

---
+++ Day 17 - Movie App/dist/script/main.js
@@ -0,0 +1,4 @@
+const api_key = '8006703c3b13fe2888b2b4c0bfcce205';
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+
