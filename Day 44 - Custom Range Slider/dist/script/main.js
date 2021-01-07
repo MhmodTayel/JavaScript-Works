07e63dd4b025c093f21a5a4a72324caf2d2eaad3
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-07 19:25:46 +0200

    add snippit ant all done

--- Day 44 - Custom Range Slider/dist/script/main.js
+++ Day 44 - Custom Range Slider/dist/script/main.js
@@ -9,3 +9,4 @@ range.addEventListener("input", () => {
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
