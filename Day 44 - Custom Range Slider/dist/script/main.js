5fe9ba4857b0626915060d50892976727125f332
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-07 19:20:09 +0200

    all done

---
+++ Day 44 - Custom Range Slider/dist/script/main.js
@@ -0,0 +1,11 @@
+const range = document.getElementById("range");
+const label = document.getElementById("label");
+
+range.addEventListener("input", () => {
+  label.innerText = range.value;
+  label.style.left = `${scale(range.value, 0, 100, -18, 259)}px`;
+});
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
