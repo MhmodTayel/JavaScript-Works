ddd1e45872392111e9da7952c952d05ac14e72e3
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-09 23:51:52 +0200

    all done

--- Day 24 - Content Placeholder/dist/script/main.js
+++ Day 24 - Content Placeholder/dist/script/main.js
@@ -1,4 +1,30 @@
 
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
+  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  excerpt.innerHTML =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+  name.innerHTML = 'John Doe'
+  date.innerHTML = 'Oct 08, 2020'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
 
 
 
@@ -13,32 +39,3 @@
 
 
 
-
-
-
-// async function getData() {
-//   const res = await fetch("https://randomuser.me/api?results=50");
-
-//   const { results } = await res.json();
-
-//   result.innerHTML = "";
-
-//   results.forEach((user) => {
-//     const li = document.createElement("li");
-//     listItems.push(li);
-
-//     li.innerHTML = `
-      
-//       <img src='${user.picture.large}' alt ='user.name.first'>
-//       <div class="user-info">
-//       <h4>${user.name.first} ${user.name.last}</h4>
-//       <p>${user.location.city}, ${user.location.country}</p>
-//       </div>
-//       <button class="message"><i class="fab fa-facebook-messenger"></i></button>
-//       `;
-
-//     result.appendChild(li);
-//   });
-// }
-
-// getData();
