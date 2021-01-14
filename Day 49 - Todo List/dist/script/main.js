313f87f5afccbe48724659c93ce4185f20f3b2bb
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-14 23:48:38 +0200

    Local Storage problem

--- Day 49 - Todo List/dist/script/main.js
+++ Day 49 - Todo List/dist/script/main.js
@@ -2,6 +2,14 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUl = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => {addTodo(todo)
+  
+  });
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -9,29 +17,55 @@ form.addEventListener("submit", (e) => {
 });
 
 function addTodo(todo) {
+  
   if (todo) {
-    const todoEl = document.createElement("li");
+    let todoText = input.value
+
+    if(todo) {
+      todoText =todo.text
+    }
 
     if (todo && todo.completed) {
       todoEl.classList.add("completed");
     }
+    const todoEl = document.createElement("li");
 
-    todoEl.innerText = todo;
+
+    todoEl.innerText = todo.text;
 
     todosUl.appendChild(todoEl);
 
-    todoEl.addEventListener("click", () =>
-      todoEl.classList.toggle("completed")
-    );
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEl.classList.add("deleted");
       setTimeout(() => {
-        todoEl.remove()
+        todoEl.remove();
+        updateLS();
       }, 500);
     });
 
     input.value = "";
+
+    updateLS();
   }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
