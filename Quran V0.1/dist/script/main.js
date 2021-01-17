72ec740806161124e58751e0c75f66b384755926
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-17 06:31:15 +0200

    need modification

--- Quran V0.1/dist/script/main.js
+++ Quran V0.1/dist/script/main.js
@@ -1,4 +1,4 @@
-const musicContainer = document.getElementById("music-container");
+const quranContainer = document.getElementById("quran-container");
 const playBtn = document.getElementById("play");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
@@ -7,67 +7,58 @@ const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 const progress = document.getElementById("progress");
 const progressContainer = document.getElementById("progress-container");
+const getSurahBtn = document.getElementById("get-surah");
 
-// songs titles
-const songs = ["hey", "summer", "ukulele"];
-
-let songIdx = 2;
-
-loadSong(songs[songIdx]);
-
-function loadSong(song) {
-  title.innerText = song;
-  audio.src = `music/${song}.mp3`;
-  cover.src = `images/${song}.jpg`;
-}
+let surahAudio = [];
+let verseIdx = 1;
 
 playBtn.addEventListener("click", () => {
-  const isPlaying = musicContainer.classList.contains("play");
+  const isPlaying = quranContainer.classList.contains("play");
   if (isPlaying) {
-    pauseSong();
+    pauseSurah();
   } else {
-    playSong();
+    playSurah();
   }
 });
 
-prevBtn.addEventListener("click", prevSong);
-nextBtn.addEventListener("click", nextSong);
+// prevBtn.addEventListener("click", prevSurah);
+// nextBtn.addEventListener("click", nextSurah);
+// audio.addEventListener("ended", nextSurah);
 audio.addEventListener("timeupdate", updateProgress);
 progressContainer.addEventListener("click", setProgress);
-audio.addEventListener("ended", nextSong);
 
-function playSong() {
-  musicContainer.classList.add("play");
+function playSurah() {
+  quranContainer.classList.add("play");
   playBtn.querySelector("i.fas").classList.remove("fa-play");
   playBtn.querySelector("i.fas").classList.add("fa-pause");
 
   audio.play();
 }
 
-function pauseSong() {
-  musicContainer.classList.remove("play");
+function pauseSurah() {
+  quranContainer.classList.remove("play");
   playBtn.querySelector("i.fas").classList.add("fa-play");
   playBtn.querySelector("i.fas").classList.remove("fa-pause");
   audio.pause();
 }
 
-function prevSong() {
-  songIdx--;
-  if (songIdx < 0) {
-    songIdx = songs.length - 1;
-  }
-  loadSong(songs[songIdx]);
-  playSong();
-}
+// function prevSurah() {
+//   surahIdx--;
+//   if (surahIdx < 0) {
+//     songIdx = songs.length - 1;
+//   }
+//   loadSong(songs[songIdx]);
+//   playSong();
+// }
 
-function nextSong() {
-  songIdx++;
-  if (songIdx > songs.length - 1) {
-    songIdx = 0;
-  }
-  loadSong(songs[songIdx]);
-  playSong();
-}
+// function nextSong() {
+//   songIdx++;
+//   if (songIdx > songs.length - 1) {
+//     songIdx = 0;
+//   }
+//   loadSong(songs[songIdx]);
+//   playSong();
+// }
 
 function updateProgress(e) {
   const { duration, currentTime } = e.srcElement;
@@ -82,4 +73,44 @@ function setProgress(e) {
   audio.currentTime = (clickX / width) * duration;
 }
 
-// Quran
+async function getSura(number) {
+  let res = await fetch(
+    `https://api.quran.sutanlab.id/surah/${number ? number : 1}`
+  );
+  let output = await res.json();
+  //  console.log(output)
+  showSura(output);
+}
+
+getSurahBtn.addEventListener("click", () => {
+  const surahNumber = surah.value;
+  if (surahNumber < 115 && surahNumber > 0) {
+    getSura(surahNumber);
+  }
+});
+
+function showSura(API_Output) {
+  const verses = API_Output.data.verses;
+  verses.forEach((verse) => {
+    surahAudio.push(verse.audio.primary);
+  });
+  console.log(surahAudio);
+  audio.src = surahAudio[0];
+  
+  audio.addEventListener("timeupdate", updateSurah);
+}
+
+// loadSurah(surahAudio[verseIdx]);
+function updateSurah(e) {
+  const { duration, currentTime } = e.srcElement;
+  console.log(duration, currentTime);
+  if (duration === currentTime ) {
+    console.log('object');
+    audio.src = surahAudio[verseIdx++]
+    audio.play();
+  }
+}
+function loadSurah(surah) {
+  // title.innerText = surah;
+  audio.src = surah;
+}
