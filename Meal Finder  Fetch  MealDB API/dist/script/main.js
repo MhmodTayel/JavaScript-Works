759e24c9e7ec5edf97a1e376cccc0a557f960318
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-18 23:44:12 +0200

    fetch meals

--- Meal Finder  Fetch  MealDB API/dist/script/main.js
+++ Meal Finder  Fetch  MealDB API/dist/script/main.js
@@ -1,26 +1,24 @@
-const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Japanese";
+const search = document.getElementById("search"),
+  submit = document.getElementById("submit"),
+  random = document.getElementById("random"),
+  mealsEl = document.getElementById("meals"),
+  resultHeading = document.getElementById("result-heading"),
+  single_mealEl = document.getElementById("single-meal");
 
-const search =document.getElementById('search'),
-      submit = document.getElementById('submit'),
-      random = document.getElementById('random'),
-      mealsEl = document.getElementById('meals'),
-      resultHeading = document.getElementById('result-heading'),
-      single_mealEl = document.getElementById('single-meal');
-
-  
- 
-      
 function searchMeal(e) {
-  e.preventDeafult();
+  e.preventDefault();
+
+  single_mealEl.innerHTML = "";
+
+  const term = search.value;
+
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  } else {
+    alert("Please enter a meal name");
+  }
 }
 
-
-
-
-
-
-
-
-submit.addEventListener('submit', searchMeal)
-
-
+submit.addEventListener("submit", searchMeal);
