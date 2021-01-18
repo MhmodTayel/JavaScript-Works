6178484c16bfb53bf3e5280357fe39e5d4aeef16
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-18 23:56:13 +0200

    fetch meals and display it

--- Meal Finder  Fetch  MealDB API/dist/script/main.js
+++ Meal Finder  Fetch  MealDB API/dist/script/main.js
@@ -15,7 +15,30 @@ function searchMeal(e) {
   if (term.trim()) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `
+        <h2>Search results for '${term}':</h2>
+        `;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML =
+            " <p>There are no search result. Try again</p>";
+          search.value = "";
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `<div class="meal">
+          <img src="${meal.strMealThumb}"></img>
+          <div class="meal-info" data-mealID = "${meal.idMeal}">
+          <h3>${meal.strMeal}</h3>
+          </div>
+          </div>`
+            )
+            .join("");
+        }
+      });
+    search.value = "";
   } else {
     alert("Please enter a meal name");
   }
