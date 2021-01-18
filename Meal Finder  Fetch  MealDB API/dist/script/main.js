e11a56da61bcf095e10fc004d4cdcdd401c60c23
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-19 00:42:50 +0200

    get all data of meal

--- Meal Finder  Fetch  MealDB API/dist/script/main.js
+++ Meal Finder  Fetch  MealDB API/dist/script/main.js
@@ -5,6 +5,48 @@ const search = document.getElementById("search"),
   resultHeading = document.getElementById("result-heading"),
   single_mealEl = document.getElementById("single-meal");
 
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  single_mealEl.innerHTML = `
+  
+  <div class="single-meal">
+  <h1>${meal.strMeal}</h1>
+  <img src="${meal.strMealThumb}"></img>
+  <div class="single-meal-info">
+  ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+  ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+  </div>
+  <div class="main">
+  <p>${meal.strInstructions}</p>
+  <h2>Ingredients</h2>
+  <ul>
+  ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+  </ul>
+  </div>
+  </div>
+  `;
+}
+
 function searchMeal(e) {
   e.preventDefault();
 
@@ -45,3 +87,17 @@ function searchMeal(e) {
 }
 
 submit.addEventListener("submit", searchMeal);
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
+});
