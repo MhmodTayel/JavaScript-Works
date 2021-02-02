1f5563665e84f516265f2a04307e145ce651b4ce
MhmodTayel <mhmodtayel0050@gmail.com>
2021-02-02 17:57:40 +0200

    text anime manipulation

--- anime JS/dist/script/main.js
+++ anime JS/dist/script/main.js
@@ -2,7 +2,17 @@ anime({
   targets: ".anime",
   translateX: 400,
   rotateZ: 360,
-  scale: 3,
+  scale: 2,
   duration: 3000,
   loop: true,
+});
+
+anime({
+  targets: "#counter",
+  value: [0, 100000],
+  duration: 3000,
+  round: 1,
+  easing: "linear",
+  loop: true,
+  direction: "alternate",
 });
