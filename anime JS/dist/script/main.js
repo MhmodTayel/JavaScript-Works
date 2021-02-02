42fa55e36f8b21b6a4985457c9495c6dc70d81cb
MhmodTayel <mhmodtayel0050@gmail.com>
2021-02-02 18:28:53 +0200

    javascript object

--- anime JS/dist/script/main.js
+++ anime JS/dist/script/main.js
@@ -1,24 +1,43 @@
-anime({
-  targets: ".anime",
-  translateX: 400,
-  rotateZ: 360,
-  scale: 2,
-  duration: 3000,
-  loop: true,
-  keyframes: [
-    { translateX: 700 },
-    { translateY: 50 },
-    { translateX: 100 },
-    { translateY: -50 },
-  ],
-});
+// anime({
+//   targets: ".anime",
+//   translateX: 400,
+//   rotateZ: 360,
+//   scale: 2,
+//   duration: 3000,
+//   loop: true,
+//   keyframes: [
+//     { translateX: 700 },
+//     { translateY: 50 },
+//     { translateX: 100 },
+//     { translateY: -50 },
+//   ],
+// });
+
+// anime({
+//   targets: "#counter",
+//   value: [0, 100000],
+//   duration: 3000,
+//   round: 1,
+//   easing: "linear",
+//   loop: true,
+//   direction: "alternate",
+// });
+
+var logEl = document.querySelector(".battery-log");
+
+var battery = {
+  charged: "0%",
+  cycles: 120,
+};
 
 anime({
-  targets: "#counter",
-  value: [0, 100000],
-  duration: 3000,
+  targets: battery,
+  charged: "100%",
+  cycles: 130,
+  duration: 50000,
   round: 1,
   easing: "linear",
-  loop: true,
-  direction: "alternate",
+  update() {
+    logEl.innerHTML = JSON.stringify(battery);
+  },
 });
