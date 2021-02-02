2d3f7506a299837e8fe36d6684dfd39196242ed9
MhmodTayel <mhmodtayel0050@gmail.com>
2021-02-02 19:10:09 +0200

    animate text path

--- anime JS/dist/script/main.js
+++ anime JS/dist/script/main.js
@@ -23,21 +23,41 @@
 //   direction: "alternate",
 // });
 
-var logEl = document.querySelector(".battery-log");
+// var logEl = document.querySelector(".battery-log");
 
-var battery = {
-  charged: "0%",
-  cycles: 120,
-};
+// var battery = {
+//   charged: "0%",
+//   cycles: 120,
+// };
+
+// anime({
+//   targets: battery,
+//   charged: "100%",
+//   cycles: 130,
+//   duration: 50000,
+//   round: 1,
+//   easing: "linear",
+//   update() {
+//     logEl.innerHTML = JSON.stringify(battery);
+//   },
+// });
+
+// anime({
+//   targets: ['.svg-attributes-demo polygon', 'feTurbulence', 'feDisplacementMap'],
+//   points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+//   baseFrequency: 0,
+//   scale: 1,
+//   loop: true,
+//   direction: 'alternate',
+//   easing: 'easeInOutExpo'
+// });
 
 anime({
-  targets: battery,
-  charged: "100%",
-  cycles: 130,
-  duration: 50000,
-  round: 1,
-  easing: "linear",
-  update() {
-    logEl.innerHTML = JSON.stringify(battery);
-  },
+  targets: '.line-drawing-demo .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
 });
