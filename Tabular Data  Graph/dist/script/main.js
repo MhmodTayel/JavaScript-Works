939c43091446a106f43fac50a52a131cb6c7a879
MhmodTayel <mhmodtayel0050@gmail.com>
2021-02-17 16:31:26 +0200

    split data

--- Tabular Data  Graph/dist/script/main.js
+++ Tabular Data  Graph/dist/script/main.js
@@ -1,7 +1,17 @@
 async function getData() {
   const res = await fetch("./script/data.csv");
   const data = await res.text();
-  console.log(data);
+  modifyData(data);
 }
 
 getData();
+
+function modifyData(data) {
+  const rows = data.split("\n").slice(1);
+  rows.forEach((element) => {
+    const row = element.split(",");
+    const year = row[0];
+    const temp = row[1];
+    console.log(year, temp);
+  });
+}
